94f40ba561b87e526ecd48e9bcbbf7a462f1952a
petrado <petka.dohnal@gmail.com>
2025-01-22 10:26:14 +0100

    data creation

---
+++ CSharp_Czechitas/Czechitas2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Czechitas2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
