23e5078ae02d43766dbca3c47dd0768082d86b9c
petrado <petka.dohnal@gmail.com>
2025-01-29 11:38:02 +0100

    opakovani

--- Czechitas4/Czechitas4/Program.cs
+++ Czechitas4/Czechitas4/Program.cs
@@ -26,7 +26,9 @@ namespace Czechitas4
 
             // collection - List
             List<Knight> knightList = new List<Knight>() { bonifac, artush, geralt };
+
             knightList.Add(maxmilian);
+            // knightList.AddRange(knightField) - add whole new collection
 
             for (int i = 0; i < knightList.Count; i++)
             {
@@ -91,12 +93,86 @@ namespace Czechitas4
 
             knightList.Clear(); // clear all list
 
+
+            List<double> cisla = new List<double>
+        {
+            0.046939913,
+            0.295866297,
+            0.852489925,
+            0.84994766,
+            0.96925183,
+            0.946275497,
+            0.688903175,
+            0.553463564,
+            0.59628254,
+            0.645816929
+        };
+
+            // Vypis vsechna cisla na konzoli (nachystej si pro to funkci)
+            Vypis(cisla);
+            Console.WriteLine();
+
+            // Vypis na konzoli pocet cisel v seznamu
+
+            Console.WriteLine($"List contains {VypisPocet(cisla)} elements");
+            Console.WriteLine();
+
+            // Pridej cislo 0.5 do seznamu
+            double addNumber = 0.5;
+            cisla.Add( addNumber );
+            Vypis(cisla);
+            Console.WriteLine();
+
+            // vypis prvni cislo ze seznamu, ktere je vetsi nez 0.8
+            double threshold = 0.8;
+            double firstBigger = 0;
+            foreach (double current in cisla)
+            {
+                if (current > threshold)
+                {
+                    firstBigger = current;
+                    break;
+                }
+            }
+            Console.WriteLine($"The first number bigger than {threshold} is {firstBigger}");
+            Console.WriteLine();
+
+            // najdi nejvetsi cislo v seznamu, vypis, ktere to je, a odstran ho ze seznamu
+            double maxValueInList = FindMaxNumbers(cisla);
+            Console.WriteLine($"Max number in the list is {maxValueInList}...");
+            cisla.Remove(maxValueInList);
+            Console.WriteLine("...and now it's gone...");
+
+
+            // vypis opet vsechna cisla a jejich pocet
+            Console.WriteLine();
+            Console.WriteLine($"Currently there is {cisla.Count} numbers in the list and the list looks like that: ");
+            Vypis(cisla);
+
+
         }
         private static int CompareKnights(Knight first, Knight second)
         {
             return string.Compare(first.Name, second.Name, true);
         }
 
+        private static void Vypis(List<double> input)
+        {
+            foreach (double current in input)
+            {
+                Console.WriteLine($"Current number is {current}");
+            }
+        }
 
+        private static int VypisPocet(List<double> input)
+        {
+            return input.Count;
+        }
+
+        private static double FindMaxNumbers(List<double> input)
+        {
+            double maxValue = input.Max();
+            return maxValue;
+        }
     }
 }
